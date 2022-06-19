39a85f084a5b9a3f6823f76c3076dd41a0e59471
DeltaZero <233a344a455@gmail.com>
2022-06-19 11:44:02 +0800

    deploy

--- deploy.bash
+++ deploy.bash
@@ -4,11 +4,11 @@
 set -e
 
 # 生成静态文件
-npm run docs:build
+# npm run docs:build
 
 git add -A
 git commit -m 'deploy'
-git push git@github.com:233a344a455/DuhangServer-doc.git master:main
+git push
 
 
 # 进入生成的文件夹
